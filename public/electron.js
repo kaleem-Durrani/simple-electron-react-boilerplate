6a8c06d98cd1f1564c32bc0f7792cb8407724a94
kaleem-Durrani <kalimdurrani22@gmail.com>
2024-05-01 11:41:15 -0700

    first commit

---
+++ public/electron.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let win;
+
+const isDev = import("electron-is-dev");
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  win.webContents.openDevTools();
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
